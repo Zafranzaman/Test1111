0d4e26ab9389f585bee1c1fb874ebe2fcc3eb775
Zafran zaman <mzafransultani56@gmal.com>
2024-08-19 13:19:21 +0500

    Initial Commit

---
+++ Test1111/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Test1111
+//
+//  Created by Zafran Mac on 19/08/2024.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ Test1111UITests/Test1111UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Test1111UITestsLaunchTests.swift
+//  Test1111UITests
+//
+//  Created by Zafran Mac on 19/08/2024.
+//
+
+import XCTest
+
+final class Test1111UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
